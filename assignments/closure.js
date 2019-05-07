467a168e333a35ba0389a40dd61dd12a34e2b275
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-07 15:36:18 +0100

    implemented closure strecth goal

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,16 +13,33 @@ console.log(sumEvenArray([1,2,3,4,5,6,7,8,9,10]));
 
 
 // ==== Challenge 2: Create a counter function ====
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0
+  function increaseCount () {
+   return count += 1;
+  }
+
+  return increaseCount;
 };
+const newCounter = counter()
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
+console.log(newCounter(),'hiiiiiii')
+console.log(newCounter(),'hiiiiiii')
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let value = 10;
+   return {
+    increment : function increment() { return value += 1},
+    decrement : function decrement() {return value -= 1}
+  }
 };
+const obj = counterFactory()
+console.log(obj.increment())
+
