41614d9d41bd3ab7310823c4dd87e94ad6375855
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-07 12:43:04 +0100

    implemented solution for closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const addFunc = (a) => a.reduce((a,b)=> a+b, 0);
+//a function that adds all even number in an array
+function sumEvenArray (arr) {
+  const even = arr.filter(num=> num%2===0);
+  return addFunc(even);
+}
 
+console.log(sumEvenArray([1,2,3,4,5,6,7,8,9,10]));
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
